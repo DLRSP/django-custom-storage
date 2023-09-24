c1ea0062af7fcea3172981c218e4154e18bc69c9
dlrsp-dev <dlrsp.dev@gmail.com>
2023-09-25 00:42:23 +0200

    - fix typo

--- src/custom_storage/storage.py
+++ src/custom_storage/storage.py
@@ -2,19 +2,19 @@ from django.core.files.storage import get_storage_class
 from storages.backends.s3 import S3Storage
 
 
-class StaticRootCachedS3torage(S3Storage):
+class StaticRootCachedS3Storage(S3Storage):
     """
     S3 storage backend that saves the files locally, too.
     """
 
     def __init__(self, *args, **kwargs):
-        super(StaticRootCachedS3torage, self).__init__(*args, **kwargs)
+        super(StaticRootCachedS3Storage, self).__init__(*args, **kwargs)
         self.location = "static"
         self.local_storage = get_storage_class("compressor.storage.CompressorFileStorage")()
 
     def save(self, name, content, max_length=None):
         self.local_storage._save(name, content)
-        super(StaticRootCachedS3torage, self).save(name, self.local_storage._open(name))
+        super(StaticRootCachedS3Storage, self).save(name, self.local_storage._open(name))
         return name
 
 
