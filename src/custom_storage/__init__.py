fbeff77be765c2d5fb014b621ba748e6a6008cc8
dlrsp-dev <dlrsp.dev@gmail.com>
2026-07-03 22:26:11 +0200

    Bump version: 0.3.4 → 0.3.5

--- src/custom_storage/__init__.py
+++ src/custom_storage/__init__.py
@@ -2,7 +2,7 @@
 See PEP 386 (https://peps.python.org/pep-0386/)
 """
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 __version_info__ = tuple(
     int(i) if i.isdigit() else i for i in __version__.split(".")
 )
